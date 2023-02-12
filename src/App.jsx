a9a14d458051ba3236d66d8e32aac7c9ef6b44fd
Alex Phillips <72033028+AlexPhillips02@users.noreply.github.com>
2023-02-12 18:04:53 +0000

    Creating basic react app

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Header from './components/header/Header.jsx'
+import Nav from './components/nav/Nav.jsx'
+import About from './components/about/About.jsx'
+
+const App = () => {
+  return (
+    <div>
+      <Header />
+      <Nav />
+      <About />
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/header/Buttons.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Buttons = () => {
+  return (
+    <div className='Buttons'>
+        <a href=''> Download CV</a>
+        <a href='#contact'> Contact Me</a>
+    </div>
+  )
+}
+
+export default Buttons
